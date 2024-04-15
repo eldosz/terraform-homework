3f41b4bdb7b54040f8efba4da23588739dea1679
eldosz <e1dos@icloud.com>
2024-04-15 05:28:21 +0000

    fixed

--- Homework4/main.tf
+++ Homework4/main.tf
@@ -20,42 +20,21 @@ resource "aws_key_pair" "web" {
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
-
   ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = var.ports[0]
+    to_port     = var.ports[1]
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-
-    ingress {
-    description      = "TLS from VPC"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-    ingress {
-    description      = "TLS from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
+  
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+}
 
-  tags = {
-  Name = "allow-tls"
-}
-}
 
 variable region {
   description = "aws region"
