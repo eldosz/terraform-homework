3137ab71d1b6c79e180de36c7fa5694234377eda
eldosz <144575101+eldosz@users.noreply.github.com>
2024-04-17 20:31:10 -0700

    Update apache2.sh

--- Homework5/apache2.sh
+++ Homework5/apache2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo yum update
-sudo yum install httpd -y
-sudo systemctl start httpd
-sudo systemctl enable httpd
+sudo apt update
+sudo apt install apache2 -y
+sudo systemctl start apache2
+sudo systemctl enable apache2
